c95ebf8056d670aeb63a2dfd57369f79c5a1f13f
zhong <4888522@qq.com>
2019-08-14 18:41:26 +0800

    add footer

---
+++ template-parts/footer/site-info.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen_child
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-info">
+    <p>大兴区第七幼儿园总园：大兴区双高小区内</p>
+    <p>大兴区第七幼儿园春天园：大兴区保利春天里小区内</p>
+    <p>京ICP备19021931号-1</p>
+</div><!-- .site-info -->
+<div class="wechat_offcial_account">
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/qrcode.jpg" alt="大兴七幼公众号" title="微信扫一扫"/>
+    <p class="caption">大兴七幼公众号</p>
+</div>
