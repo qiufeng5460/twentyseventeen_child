05d3e5a94eb05de04b0a59d7c114f9291745c2ca
zhong <4888522@qq.com>
2019-11-26 14:51:53 +0800

    根据工信部要求：已通过备案的用户，在网站开通时应在网站首页底部放置正确的备案号，并链接到工信部网址：beian.miit.gov.cn，供公众查询核对。

--- template-parts/footer/site-info.php
+++ template-parts/footer/site-info.php
@@ -12,7 +12,8 @@
 <div class="site-info">
     <p>大兴区第七幼儿园总园：大兴区双高小区内</p>
     <p>大兴区第七幼儿园春天园：大兴区保利春天里小区内</p>
-    <p>京ICP备19021931号-1</p>
+    <p><a href="http://beian.miit.gov.cn/" target="_blank">京ICP备19021931号-1</a></p>
+    
 </div><!-- .site-info -->
 <div class="wechat_offcial_account">
     <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/qrcode.jpg" alt="大兴七幼公众号" title="微信扫一扫"/>
